2afb1957fb4c46436804e4f461150878db6519a5
Claudio <klauddio.silva@gmail.com>
2025-11-09 17:34:48 -0300

    JPA Repository, dependency injection, database seeding

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,18 +1,26 @@
 package com.educandoweb.course.resources;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "Maria", "maria@gmail.com", "888999", "12345");
-        return ResponseEntity.ok(user);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = userRepository.findAll();
+        //User user = new User(1L, "Maria", "maria@gmail.com", "888999", "12345");
+        return ResponseEntity.ok(users);
     }
 }
